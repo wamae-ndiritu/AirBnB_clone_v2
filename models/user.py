f508027b1236131a7d7484f2326e79f5289ccd39
kahenyamercy <kahenyamercy5@gmail.com>
2023-11-22 21:54:17 +0000

    revert changes in user.py

--- models/user.py
+++ models/user.py
@@ -1,29 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
-
-<<<<<<< HEAD
-class User(BaseModel, Base):
-    """This class defines a user by various 
-    Attributes:
-        email: email address
-        password: password for your login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan', backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan', backref="user")
-=======
+from models.base_model import BaseModel
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
@@ -31,4 +8,3 @@ class User(BaseModel):
     password = ''
     first_name = ''
     last_name = ''
->>>>>>> parent of 46146d0... update user to be in dbs
