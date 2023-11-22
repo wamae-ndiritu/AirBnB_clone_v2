46146d0c7b80bcdab0cb90086f9bcdc3c18c3e6c
kahenyamercy <kahenyamercy5@gmail.com>
2023-11-22 13:48:01 +0000

    update user to be in dbs

--- models/user.py
+++ models/user.py
@@ -3,9 +3,20 @@
 from models.base_model import BaseModel
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel Base):
+    """This class defines a user by various 
+    Attributes:
+	email: email address
+	password: password for your login
+	first_name: first name
+	last_name: last name
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+			  backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+			   backref="user")
