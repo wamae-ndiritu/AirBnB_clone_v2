50855b81ae49d981da63bc07ef67ccd14f3fe283
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-11-23 02:25:01 +0000

    Update User model to be a db model

--- models/user.py
+++ models/user.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from models import storage_type
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
