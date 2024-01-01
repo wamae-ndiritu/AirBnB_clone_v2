41891835de7faa4f19852b3e076afc34e9ebda49
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-01-01 20:52:45 +0000

    Add route that takes an integer parameter

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+A script to start a Flask web application.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays hello hbnb
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays 'HBNB'
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """
+    display 'C' followed by the value of
+    the text variable (replace
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def display_python(text='is cool'):
+    """
+    displays 'Python', followed by the value of the text
+    variable (replace underscore _ symbols with a space )
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """
+    display “n is a number” only if n is an integer
+    """
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
