f1ed08b4278c2740afdac9aa1c6857a53e3870d1
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-01-01 20:36:51 +0000

    Add route to pass text passed as parameter

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A script to start a Flask web application.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays hello hbnb
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays 'HBNB'
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """
+    display 'C' followed by the value of
+    the text variable (replace
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
